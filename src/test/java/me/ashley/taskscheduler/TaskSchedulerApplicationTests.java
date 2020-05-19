2d6d34c5b6a17cd80293b390751fd4f2aa5566fd
yj <>
2020-05-19 23:10:30 +0800

    Initial commit

---
+++ src/test/java/me/ashley/taskscheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ashley.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
